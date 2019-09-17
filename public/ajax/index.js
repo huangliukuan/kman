13666bc6453f3eb8471ef2502b88abe877f4bb81
kuan <kuan@qq.com>
2019-09-17 21:01:58 +0800

    2019/9/17/21:00

--- public/ajax/index.js
+++ public/ajax/index.js
@@ -1,3 +1,83 @@
+// 轮播图
+ //图片下标位置
+ var i=0;
+ //每次轮播长度
+ var LIWIDTH=1184;
+ // 每次自动轮播默认时间
+ var DURATION=500;
+ //li的数量
+ var LICOUNT=4;
+ //获得了轮播图的ul
+ var ulImgs=document.querySelector("section .carousel>.carCont>#ulImg");
+ // 获得小圆点ul
+ var ulIdxs=document.querySelector("section .carousel>.carCont>#ulIdxs");
+// 获得小圆点
+var lis=ulIdxs.children;
+function moveTo(to){
+	//如果没有给to赋值,则i+1,为让图片播放下一张
+	if(to==undefined){
+		to=i+1;
+	}
+	if(i==0){
+		if(to>i){
+			ulImgs.className="trans";
+		}else{
+			ulImgs.className="";
+			ulImgs.style.marginLeft=-LIWIDTH*LICOUNT+"px";
+			setTimeout(function(){
+				moveTo(LICOUNT-1);
+			},100);
+			return;
+		}
+	}
+	i=to;
+	ulImgs.style.marginLeft=-i*LIWIDTH+"px";
+	for(var li of lis){
+		li.className="";
+	}
+	if(i==LICOUNT){
+		i=0;
+		setTimeout(function(){
+			ulImgs.className="";
+			ulImgs.style.marginLeft=0;
+		},DURATION)
+	}
+	lis[i].className="active";
+}
+//左右按钮
+var btnNext=document.querySelector("section .carCont>.btnNext");
+var btnPrev=document.querySelector("section .carCont>.btnPrev");
+var canClick=true;
+function move(n){
+	if(canClick){
+		moveTo(i+n);
+		canClick=false;
+		setTimeout(function(){
+			canClick=true;
+		},DURATION+100);
+	}
+}
+btnPrev.onclick=function(){
+	move(-1);
+}
+btnNext.onclick=function(){
+	move(1);
+}
+// 自动轮播
+var times=3000;
+var timer=setInterval(function(){
+	moveTo();
+},times);
+
+var carCont=document.querySelector("section .carousel>.carCont");
+carCont.onmouseover=function(){
+	clearInterval(timer);
+}
+
+
+
+
+
 // 1.查找触发事件的元素
 // 查找所有带有data-click属性,且属性值为tab的元素
 var btns=document.querySelectorAll("[data-click=tab]");
