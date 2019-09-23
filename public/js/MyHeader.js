c06c9f6cf12b2475dd310631436e898e23ff5d6f
kuan <kuan@qq.com>
2019-09-23 20:51:48 +0800

    2019/9/23/21:00

---
+++ public/js/MyHeader.js
@@ -0,0 +1,43 @@
+var MyHeader={
+  template:`<div class="hbody">
+  <div class="logo float">
+    <a href="#"><img src="image/logo.jpg" alt="logo"></a>
+  </div>
+  <div class="direc float">
+    <ul>
+      <li>
+        <a href="#">首页</a>
+      </li>					
+      <li>
+        <a href="#">分类</a>
+      </li>					
+      <li>
+        <a href="#">排行榜</a>
+      </li>					
+      <li>
+        <a href="#">APP下载</a>
+      </li>					
+      <li>
+        <a href="#">我要投稿</a>
+      </li>					
+      <li>
+        <a href="#">动漫</a>
+      </li>					
+      <li>
+        <a href="#">游戏中心</a>
+      </li>					
+      <li>
+        <a href="#">条漫大赛</a>
+      </li>
+    </ul>
+  </div>
+  <div class="float">
+    <input class="search" type="text" placeholder="搜索作品、作者名">
+    <a href="#"><img class="isearch" src="image/search.png" alt="搜索"></a>
+  </div>
+  <div class="user float">
+    <a href="">登录</a>|
+    <a href="">注册</a>
+  </div>
+</div>`
+}
